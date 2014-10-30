c3e0d72f04e71848c1206b5143bc890ca10b473d
Daniel Sun <dsun@lab49.com>
2014-10-30 09:23:22 -0400

    added saucelabs temp account name and key

--- protractor_simpletest/conf.js
+++ protractor_simpletest/conf.js
@@ -1,5 +1,5 @@
 // conf.js
-// This configuration tells Protractor where your test files (specs) are, and where to talk to your Selenium Server (seleniumAddress). 
+// This configuration tells Protractor where your test files (specs) are, and where to talk to your Selenium Server (seleniumAddress).
 // It will use the defaults for all other configuration. Chrome is the default browser.
 
 // You can run the test by executing the folliwng line on your terminal:
@@ -11,7 +11,7 @@
 // Protractor test!
 
 exports.config = {
-  sauceUser: 'albedith',
-  sauceKey: '************************',
+  sauceUser: 'sudaniatgs',
+  sauceKey: 'd20339c5-6219-4689-93b7-7bf9616bc25d',
   specs: ['spec.js']
 }
